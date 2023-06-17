de1c2b961ff1551867e2d9369a6dea2828aabd68
Skaps664 <msudaisk664@gmail.com>
2023-06-17 14:34:09 +0500

    Implemented basic CR of CRUD and we can get and post

--- server.js
+++ server.js
@@ -2,17 +2,47 @@ const { error } = require("console");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+
+app.use(express.json());
 
 //routes
 app.get("/", (req, res) => {
-  res.send("Hey Node api");
+  res.send("Hey Node API");
 });
 app.get("/blog", (req, res) => {
   res.send("This is where i will post my blogposts");
 });
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
