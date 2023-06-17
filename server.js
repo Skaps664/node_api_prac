4a3702a160a4b874f4de8f224cba977e4565a01f
Skaps664 <msudaisk664@gmail.com>
2023-06-17 17:58:14 +0500

    complete basic CRUD operation api

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const Product = require("./models/productModel");
 
 app.use(express.json());
 
-//routes
+// GET routes (read)
 app.get("/", (req, res) => {
   res.send("Hey Node API");
 });
+
 app.get("/blog", (req, res) => {
   res.send("This is where i will post my blogposts");
 });
 
+// GET json from db
 app.get("/products", async (req, res) => {
   try {
     const products = await Product.find({});
@@ -24,6 +26,7 @@ app.get("/products", async (req, res) => {
   }
 });
 
+// getting json data bt id from our db
 app.get("/products/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -35,8 +38,10 @@ app.get("/products/:id", async (req, res) => {
   }
 });
 
+// POST route (create)
 app.post("/products", async (req, res) => {
   try {
+    // posting json onto our db
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
@@ -45,6 +50,43 @@ app.post("/products", async (req, res) => {
   }
 });
 
+// PUT route (update)
+app.put("/products/:id", async (req, res) => {
+  try {
+    // updating json in our db
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete a product
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Configuring MongoDB and Starting our server
 mongoose
   .connect(
     "mongodb+srv://admin:123456789Admin@nodeapi.kpgbi78.mongodb.net/NODE_API?retryWrites=true&w=majority"
